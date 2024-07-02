4af7004c26ec3a32cb43e5eb343e484775a373d7
vasujain275 <vasujain275@gmail.com>
2024-07-02 21:35:27 +0530

    feat: added drizzle and turso

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -1,1 +1,3 @@
 export const PORT = Number.parseInt(process.env.PORT || "8069", 10);
+export const DB_URL = process.env.DB_URL as string;
+export const DB_AUTH_TOKEN = process.env.DB_AUTH_TOKEN as string;
