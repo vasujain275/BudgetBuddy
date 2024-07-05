e81e4e0b9282c97f23621732a11c3b28740b5bdd
vasujain275 <vasujain275@gmail.com>
2024-07-05 06:59:34 +0530

    feat: made navbar responsive

--- web/src/lib/components/custom/NavBar.svelte
+++ web/src/lib/components/custom/NavBar.svelte
@@ -4,19 +4,21 @@
   import ProfileIcon from "$lib/components/custom/ProfileIcon.svelte";
 </script>
 
-<nav class="mt-2 grid grid-cols-6 md:grid-cols-12">
-  <div class="col-span-1 flex items-center justify-end gap-2 md:col-span-2">
+<nav class="grid grid-cols-6 gap-4 p-4 md:grid-cols-12">
+  <div class="col-span-1 flex items-center gap-2 md:col-span-2">
     <img class="h-12 w-12" src="logo.png" alt="logo" />
-    <a href="/" class="hidden md:block">BudgetBuddy</a>
+    <a href="/" class="hidden text-lg font-bold md:block">BudgetBuddy</a>
   </div>
-  <div class="ml-4 hidden items-center justify-center md:col-span-8 md:flex">
+
+  <div class="hidden items-center justify-center gap-4 md:col-span-8 md:flex">
     <Button variant="ghost" class="text-lg">Overview</Button>
     <Button variant="ghost" class="text-lg">Transactions</Button>
     <Button variant="ghost" class="text-lg">Budget</Button>
   </div>
-  <div class="text-offwhite col-span-6 ml-4 flex items-end md:col-span-2 md:items-center">
+
+  <div class="col-span-5 flex items-center justify-end gap-4 md:col-span-2">
     <ThemeButton />
-    <div class="ml-4 mr-4">
+    <div class="ml-4">
       <ProfileIcon />
     </div>
   </div>
