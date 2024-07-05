a0ae1aa4a2625973bfd96e1246610f24350c7450
vasujain275 <vasujain275@gmail.com>
2024-07-06 01:50:19 +0530

    feat: moved to blue shadcn theme

--- web/src/routes/login/schema.ts
+++ web/src/routes/login/schema.ts
@@ -14,11 +14,11 @@ export const loginFormSchema = z.object({
       invalid_type_error: "Password must be a string"
     })
     .min(8, { message: "Password must be at least 8 characters long" })
-    .max(32, { message: "Password must be at most 32 characters long" })
-    .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
-    .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
-    .regex(/[0-9]/, { message: "Password must contain at least one number" })
-    .regex(/[^a-zA-Z0-9]/, { message: "Password must contain at least one special character" })
+  //.max(32, { message: "Password must be at most 32 characters long" })
+  //.regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+  //.regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+  //.regex(/[0-9]/, { message: "Password must contain at least one number" })
+  //.regex(/[^a-zA-Z0-9]/, { message: "Password must contain at least one special character" })
 });
 
 export type LoginFormSchema = z.infer<typeof loginFormSchema>;
